81e421a751f1eb30e5df8b34fa686a164c21fb16
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-21 22:25:24 -0400

    Correcting styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,42 @@
-function eliminarElemento(id){
-	imagen = document.getElementById(id);	
-	if (!imagen){
-		alert("El elemento selecionado no existe");
-	} else {
-		padre = imagen.parentNode;
-		padre.removeChild(imagen);
-	}
-}
+
+function la_img()
+        {
+            var nuevoLi=document.getElementById("ima_gen").value;
+            if(nuevoLi.length>0)
+            {
+                if(find_li(nuevoLi))
+                {
+                    var li=document.createElement('li');
+                    li.id=nuevoLi;
+                    li.innerHTML="<span onclick='eliminar(this)'>X</span>"+nuevoLi;
+                    document.getElementById("listaDesordenada").appendChild(li);
+                }
+            }
+            return false;
+        }
+ 
+        /**
+         * Funcion que busca si existe ya el <li> dentrol del <ul>
+         * Devuelve true si no existe.
+         */
+        function find_li(contenido)
+        {
+            var el = document.getElementById("listaDesordenada").getElementsByTagName("li");
+            for (var i=0; i<el.length; i++)
+            {
+                if(el[i].innerHTML==contenido)
+                    return false;
+            }
+            return true;
+        }
+ 
+        /**
+         * Funcion para eliminar los elementos
+         * Tiene que recibir el elemento pulsado
+         */
+        function eliminar(elemento)
+        {
+            var id=elemento.parentNode.getAttribute("id");
+            node=document.getElementById(id);
+            node.parentNode.removeChild(node);
+        }
